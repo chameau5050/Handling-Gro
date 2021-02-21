50d711c8fefd1aaa6f64ff793fbcff5f07f69dd0
chameau5050 <54971185+chameau5050@users.noreply.github.com>
2021-02-21 17:04:59 -0500

    Add tcp/ip on python side (#28)
    * add frame and accumulator to python side
    * add EthernetComModule
    * add server methode

--- Application/test/test_main.py
+++ Application/test/test_main.py
@@ -3,9 +3,9 @@ import unittest
 from test.TestComm.testCommDevice import *
 from test.TestComm.testControlMessage import *
 from test.TestComm.testMessageIO import *
-
-
-
+from test.TestComm.testFrame import *
+from test.TestComm.testAccumulator import *
+from test.TestComm.testEthernetComm import *
 
 if __name__ == '__main__':
     unittest.main()
