330737f61b7100cb9ac286968b73c8e6538075cb
gigj1302 <55798666+gigj1302@users.noreply.github.com>
2021-03-30 13:00:05 -0400

    Add Unitest for StepperJoin

--- MotorControle/Platform_IO_Board_Motor_Controle/test/test_main.cpp
+++ MotorControle/Platform_IO_Board_Motor_Controle/test/test_main.cpp
@@ -2,6 +2,7 @@
 #include <unity.h>
 #include "testComm/testComm.h"
 #include "Test_Join.h"
+#include "Test_StepperJoin.h"
 #include "Test_DriveManager.h"
 #include "Test_StateManager.h"
 
@@ -18,12 +19,14 @@ void loop()
     //initialisation
     TestComm test_comm;
     Test_Join test_join;
+    Test_StepperJoin test_stepperjoin;
     Test_DriveManager test_drivemanager;
     Test_StateManager test_StateManager;
 
     //launch all test
     test_comm.test();
     test_join.test();
+    test_stepperjoin.test();
     test_drivemanager.test();
     test_StateManager.test();
 
