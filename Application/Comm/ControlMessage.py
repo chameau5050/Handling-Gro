e30e81bb14e8ddb5c304c6c58cc4a2458f61b9e9
chameau5050 <54971185+chameau5050@users.noreply.github.com>
2021-04-05 16:02:50 -0400

    create robotAPI class (#64)
    * create robotAPI class

--- Application/Comm/ControlMessage.py
+++ Application/Comm/ControlMessage.py
@@ -1,5 +1,12 @@
 class ControlMessage:
-    def __init__(self,type,payload=[]):
+    SET_HOME = 3
+    SET_JOIN_POSITION = 7
+    SET_CARTESIAN_POSITION = 8
+    GOTO_HOME = 9
+    OPEN_GRIPPER = 10
+    CLOSE_GRIPPER = 12
+
+    def __init__(self, type, payload=[]):
         self.type = type
         self.payload = payload
 
