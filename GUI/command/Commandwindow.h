70d248fdcba209c9748ada3cd6780cbbf3b1dd56
chameau5050 <54971185+chameau5050@users.noreply.github.com>
2021-04-15 21:41:33 -0400

    Final UI  (#73)
    * Redesigned UI
    
    - Changed sliders to spin boxes
    - Added speed spin boxes
    - Added tab windows
    - Changed window layout
    - Changed text font
    - Added buttons for future features
    - Added tab for adding features for saving/loading files
    
    * Added cartesian and home features
    
    - Added input for cartesian coordinates;
    - Changed Speed section to Cartesian section;
    - Added set home and go to home features;
    - Changed spin boxes to double spin boxes;
    - Added buttons and plaintextedit for Save/Load tab
    
    * Added load/save/run file functions
    
    - Added load file function
    - Added save file function
    - Added run file function (UNFINISHED)
    - Added buttons in UI for new functions
    - Added display of loaded file for display and editing
    TO DO:
    - For run file function, add code for opening and closing the gripper
    - For run file function, be able to send message containing a float type payload
    
    * update comm
    
    * Added quality of life functions
    
    - Added buttons that automatically write lines of code in the display;
    - Added a zerotage function;
    - Added buttons to open/close gripper;
    - Added error message if command is not recognized;
    - Added buttons to save and load files via a directory window;
    - Changed the display of the parameters window to only show the relevant fields depending of the mode
    
    * add controlMessage floating point
    
    * allow negative number in spin box
    
    * Corrected messages to send floating point data
    
    * remove ui attifact
    
    * adapt UI
    
    * Small corrections
    
    - Changed code to fit with gripper slider;
    - Hid the close gripper button in parameters;
    - Added a file example for save/load/run functions
    
    Co-authored-by: Theparky105 <54992154+Theparky105@users.noreply.github.com>

--- GUI/command/Commandwindow.h
+++ GUI/command/Commandwindow.h
@@ -24,14 +24,38 @@ public:
     ~CommandWindow();
     void sendJSon(QJsonDocument doc);
     void sendControlMessage(ControlMessage* msg);
+    void sendControlMessage(ControlMessagefloatingPoint* msg);
+
     bool messageReceved(char* rawDataFrame,unsigned int lenght);
+    bool sendCartesianPosition(QStringList command);
+    bool sendJointPosition(QStringList command);
+
 public slots:
-    void sendJointPosition();
+    void sendJointParameters();
+    void setHome();
+    void goHome();
     void connectSocket();
     void readData();
+    void loadFile();
+    void saveFile();
+    void runFile();
+    void openFile();
+    void saveFileAs();
+    void zeroIn();
+    void commandMoveHere();
+    void commandGoHome();
+    void commandSetHomeHere();
+    void commandOpenGripper();
+    void commandCloseGripper();
+    void hideJoint();
+    void hideCartesian();
+    void openGripper();
+    void closeGripper();
 private:
     Ui::CommandWindow *ui;
     QTcpSocket* socket;
     Accumulator* acc;
+    float home_joint[4] = {0, 0, 0, 0};
+    float home_cart[3] = {0, 0, 0};
 };
 #endif // COMMANDWINDOW_H
